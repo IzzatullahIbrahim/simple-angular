3293d4a66f46a77146401472b7568b9ed3e5f4f7
Zack <izzatullahibrahim@yahoo.com>
2016-12-19 15:53:19 -0700

    Setting up Angular

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,30 @@
-console.log ('Hello World')
+'use strict';
+
+// Create our 'MyApp angular application with the
+//'ui.router' as a dependency.
+var app = angular.module ('MyApp',['ui.router']);
+
+//configure the 'myApp' angular application
+app.config ([
+    //Bring in the dependency objects by name.
+    '$stateProvider',
+
+    //Provide a callback function tp use one all
+    //the dependencies have been loaded.
+    function ($stateProvider){
+        //Setup and define our page states.
+        $stateProvider
+            .state('home',{
+                url:'/',
+                template: '<h2>Home Page</h2>'
+            })
+            .state('about',{
+                url:'/about',
+                template: '<h2>About Page</h2>'
+            })
+            .state('contact',{
+                url:'/contact',
+                template: '<h2>Contact Page</h2>'
+            })
+    }
+])
